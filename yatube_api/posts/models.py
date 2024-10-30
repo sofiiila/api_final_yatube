a13e3741696f1b61b95b6e4fc409e71070a7a991
sofi <@sofiiila>
2024-10-30 21:51:25 +0300

    моделька Follow

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,7 +1,35 @@
 from django.contrib.auth import get_user_model
+
 from django.db import models
 
 User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(
+        'Название группы',
+        max_length=200,
+        help_text='Укажите название группы',
+    )
+
+    slug = models.SlugField(
+        'Адресс группы',
+        max_length=200,
+        unique=True,
+        help_text='Укажите адресс группы',
+    )
+
+    description = models.TextField(
+        'Описание группы',
+        help_text='Опишите группу',
+    )
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
@@ -11,6 +39,15 @@ class Post(models.Model):
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Группа',
+        blank=True,
+        null=True,
+        help_text='Выберите группу',
+    )
 
     def __str__(self):
         return self.text
@@ -24,3 +61,26 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name='follower'
+    )
+
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name='following'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        unique_together = ("user", "following")
