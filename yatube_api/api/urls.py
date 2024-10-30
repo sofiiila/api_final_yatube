0a3395705806ac65685feb66863a7f18194603ca
sofi <@sofiiila>
2024-10-30 22:17:34 +0300

    ispravila oshibky permis

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet, basename='posts')
+router.register(r'posts/(?P<post_id>\d+)/comments', views.CommentViewSet,
+                basename='comments')
+router.register(r'groups', views.GroupViewSet, basename='groups')
+router.register(r'follow', views.FollowViewSet, basename='follows')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls.jwt')),
 ]
